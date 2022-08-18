30f03244a06e36e332a5a57b3c36cffded59fc3c
KanoCode <kanokanombola@gmail.com>
2022-08-18 13:57:55 +0200

    Add update post counter, recent_comments method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+
+  after_save :updates_post_counter
+
+  def updates_post_counter
+    user.increment!(:postsCounter)
+  end
+
+  def recent_comments
+    comments.limit(5).order(created_at: :desc)
+  end
 end
