11986490d6e6d7abb4c79f412bd428bb6ae32f22
KanoCode <kanokanombola@gmail.com>
2022-08-19 00:28:08 +0200

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  validates :name, presence:true
-  validates  :postsCounter, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+  validates :postsCounter, numericality: { greater_than_or_equal_to: 0 }
   def recent_posts
     posts.limit(3).order(created_at: :desc)
   end
--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
-
 describe User do
-    #test
-    first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+  # test
+  first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
 
-    subject { first_user}
+  subject { first_user }
 
-    before {subject.save}
+  before { subject.save }
 
-    it 'name should be present' do
-        subject.name = nil 
-        expect(subject).to_not be_valid
-    end
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 
-    it 'postsCounter must be an integer' do
-        subject.postsCounter = 1
-        expect(subject.postsCounter).to be >= 0
-    end
+  it 'postsCounter must be an integer' do
+    subject.postsCounter = 1
+    expect(subject.postsCounter).to be >= 0
+  end
 end
