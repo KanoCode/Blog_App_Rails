42b6a866667f180f3ea113cda52a09aa71d17b56
KanoCode <kanokanombola@gmail.com>
2022-08-18 23:16:59 +0200

    Add validations for our data

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presece:true
+  validates :title, length: { maximum: 250 }
+  validates  :commentsCounter, numericality: {  only_integer: true , greater_than_or_equal_to: 0 }
+  validates  :likesCounter, numericality: {  only_integer: true , greater_than_or_equal_to: 0 }
+ 
   after_save :updates_post_counter
 
   def updates_post_counter
