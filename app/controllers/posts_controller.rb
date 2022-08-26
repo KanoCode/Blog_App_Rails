28a3e6e4fc4c1d7fc0c11f7b85d417a5107f5224
KanoCode <kanokanombola@gmail.com>
2022-08-26 23:18:29 +0200

    Add views for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,19 @@
 class PostsController < ApplicationController
-  def index; end
 
-  def show; end
+  before_action :set_user
+
+  def index
+        @posts = @user.posts
+  
+  end
+
+  def show
+    @post = @user.posts.find(params[:id])
+  end
+
+  private 
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
 end
